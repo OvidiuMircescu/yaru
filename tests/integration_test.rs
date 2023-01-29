7598550231f243352b69305a3968dbe30fb4fb06
Ovidiu Mircescu <ovidiu.mircescu@edf.fr>
2023-01-29 23:23:47 +0100

    Tests improved.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -9,6 +9,14 @@ fn newtask(message:&str, deps:&[usize], clown:&Rc<RefCell<String>>, sched:&mut n
     sched.submit(Box::new(task))
 }
 
+fn newtask_vec(message:&str, deps:&[usize], clown:&Rc<RefCell<Vec<String>>>, sched:&mut newsched::Scheduler ) ->usize{
+    let clown = clown.clone();
+    let message = String::from(message);
+    let func = move || clown.borrow_mut().push(String::from(&message));
+    let task = newsched::SimpleTask::new(&deps, Box::new(func));
+    sched.submit(Box::new(task))
+}
+
 #[test]
 fn one_task(){
     let result = Rc::new(RefCell::new(String::new()));
@@ -61,42 +69,57 @@ fn one_depends_on_many(){
 
 #[test]
 fn chain(){
-    let result = Rc::new(RefCell::new(String::new()));
+    let result = Rc::new(RefCell::new(Vec::new()));
     let mut sched = newsched::Scheduler::new();
 
-    let mut expected = String::from("start task\n");
-    let mut idx = newtask(&expected, &[], &result, &mut sched);
+    let start = String::from("start task\n");
+    let mut idx = newtask_vec(&start, &[], &result, &mut sched);
     let start_task_id = idx;
     let mut deps = Vec::new();
 
     // first set of tasks depending on start
+    let mut exp_set1 = Vec::new();
     for _ in 1..=3{
         let message = format!("first set task {}\n", idx);
-        expected.push_str(&message);
-        idx = newtask(&message, &[start_task_id], &result, &mut sched);
+        exp_set1.push(message.clone());
+        idx = newtask_vec(&message, &[start_task_id], &result, &mut sched);
         deps.push(idx);
     }
 
     // middle set of tasks depending on first set
-    let mut middleset = String::new();
+    let mut middleset = Vec::new();
     for _ in 1..=3{
         let message = format!("depends on first set task {}\n", idx);
-        middleset.push_str(&message);
-        idx = newtask(&message, &deps, &result, &mut sched);
+        middleset.push(message.clone());
+        idx = newtask_vec(&message, &deps, &result, &mut sched);
     }
 
     // second of tasks set depending on start
+    let mut exp_set2 = Vec::new();
     for _ in 1..=3{
         let message = format!("second set task {}\n", idx);
-        expected.push_str(&message);
-        idx = newtask(&message, &[start_task_id], &result, &mut sched);
+        exp_set2.push(message.clone());
+        idx = newtask_vec(&message, &[start_task_id], &result, &mut sched);
     }
 
     sched.start();
 
-    expected.push_str(&middleset);
-    println!("{}", *result.borrow());
-    // TODO test
-    // assert_eq!(*result.borrow(), expected);
+    assert_eq!(result.borrow().get(0), Some(&start));
+    let mut maxset1 = 0usize;
+    for t in &exp_set1{
+        assert!(result.borrow().contains(t));
+        let idx = result.borrow().iter().position(|x| x == t).unwrap();
+        if idx > maxset1 {
+            maxset1 = idx;
+        }
+    }
+    for t in &exp_set2{
+        assert!(result.borrow().contains(t));
+    }
+    for t in &middleset{
+        assert!(result.borrow().contains(t));
+        let idx = result.borrow().iter().position(|x| x == t).unwrap();
+        assert!(idx > maxset1);
+    }
 }
 
